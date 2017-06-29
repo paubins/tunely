c6620afa27230acd7b9ae8065060c81c593974c2
paubins <paubin12@gmail.com>
2017-06-29 08:31:32 -0700

    Initial Commit

---
+++ Video Tuner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Video Tuner
+//
+//  Created by Patrick Aubin on 6/29/17.
+//  Copyright © 2017 com.paubins.VideoTuner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
